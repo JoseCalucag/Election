ca40d4dbb5848489bcc9eb079cc54d14b9824832
Jose Calucag <josecalucag@Joses-MacBook-Pro.local>
2020-09-13 05:45:09 -0400

    Adding the election audit code.

--- Python_practice.py
+++ Python_practice.py
@@ -6,10 +6,10 @@ counties.pop(3)
 
 counties_tuple = ("Arapahoe","Denver","Jefferson")
 
-#counties_dict = {}
-#counties_dict["Arapahoe"] = 422829
-#counties_dict["Denver"] = 463353
-#counties_dict["Jefferson"] = 432438
+counties_dict = {}
+counties_dict["Arapahoe"] = 422829
+counties_dict["Denver"] = 463353
+counties_dict["Jefferson"] = 432438
 
 #len(counties_dict)
 #counties_dict.items()
@@ -63,13 +63,3 @@ for voters in counties_dict.values():
 
 #for county, voters in counties_dict.items():
 #   print (county + " county has " + str(voters) + " registered voters.")
-
-
-counties_dict = {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
-for county, voters in counties_dict.items():
-   print (f county + " county has " + {voters:,} + " registered voters")
-
-
-voting_data = [``{"county":"Arapahoe", "registered_voters": 422829}, {"county":"Denver", "registered_voters": 463353}, {"county":"Jefferson", "registered_voters": 432438}]
-for county, registered_voters in voting_data():
-   print(     " county has " + {voters:,} + " registered voters.")
